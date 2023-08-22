288a43fcfd78ec400c07484b5a9b16ddfb8cdfcb
Simon Junior Ndovi <simonjuniorndovi@gmail.com>
2023-08-22 14:15:49 -0700

    number of elements of the array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array to be printed
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0 ; c < n ; c++)
+	{
+		printf("%d", a[c]);
+		if (c < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
